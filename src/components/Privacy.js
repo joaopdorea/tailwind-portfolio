549822741aa6a7e3b857e1a03eed584db45dadef
Joao Dorea <joaodorea300@gmail.com>
2025-05-15 15:53:25 -0300

    formatando as infos de privacidade

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -8,13 +8,17 @@ const Privacy = () => {
     <div className=" text-white bg-[#141414]" id ="experiences">
 
 <div className="justify-between items-center h-[1200px] lg:h-[850px] max-w-[1240px] mx-auto px-4 bg-[#141414] pt-[50px] sm:pt-[100px]">
-    <b className="text-3xl bg-[#141414]"> Privacy </b>
+
 
 <p className="text-xl bg-[#141414] ml-5 w-[90%]"> Política de Privacidade
 A sua privacidade é muito importante para nós. Este aplicativo coleta apenas as informações necessárias para oferecer uma experiência segura, personalizada e eficiente aos usuários. Todas as informações são tratadas com responsabilidade, seguindo as normas de proteção de dados vigentes.
 
+</p>
+<h2>
 1. Dados Pessoais
+</h2>
 
+<p>
 Coletamos alguns dados pessoais, como nome, e-mail e número de telefone, com o objetivo de:
 
 Personalizar a sua experiência no aplicativo;
@@ -22,20 +26,30 @@ Facilitar a comunicação com você;
 Garantir a segurança de sua conta e de suas transações.
 Esses dados nunca são vendidos ou compartilhados com terceiros sem o seu consentimento.
 
+</p>
+<h2>
 2. Dados de Localização
+</h2>
 
+<p>
 O app pode solicitar acesso à sua localização para:
 
 Melhorar a experiência de uso, como sugerir opções mais relevantes próximas de você;
 Otimizar funcionalidades baseadas em geolocalização, como rotas ou serviços locais.
 A coleta de localização é feita somente com sua permissão e pode ser desativada a qualquer momento nas configurações do seu dispositivo.
-
+</p>
+<h2>
 3. Segurança
+</h2>
 
+<p>
 Adotamos medidas técnicas e organizacionais adequadas para proteger seus dados contra acessos não autorizados, vazamentos ou usos indevidos.
-
+</p>
+<h2>
 4. Transparência e Controle
+</h2>
 
+<p>
 Você tem o direito de acessar, corrigir ou excluir seus dados a qualquer momento. Também pode entrar em contato conosco para esclarecer dúvidas ou exercer seus direitos relacionados à privacidade.
 
  </p>
