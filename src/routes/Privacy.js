9a39097981f083032129dbe69a015fdb43d700e2
Joao Dorea <joaodorea300@gmail.com>
2025-05-15 15:49:08 -0300

    teste final 2

--- src/routes/Privacy.js
+++ src/routes/Privacy.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import University from '../components/University'
-import Privacy from '../components/Finance'
+import Privacy from '../components/Privacy'
 
 const PrivacyPage = () => {
   return (
